8ac599f4690e240ab0b7d8cb6306c39bd20f80c0
Anton Tran <anton.tran33@gmail.com>
2025-09-05 22:36:08 +0200

    initialize project

---
+++ src/test/java/dat250/experiment/pollapp/PollappApplicationTests.java
@@ -0,0 +1,13 @@
+package dat250.experiment.pollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
