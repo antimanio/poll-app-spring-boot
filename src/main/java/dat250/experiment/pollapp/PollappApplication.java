fbd0f676c39ce0c17bd662a5c579d08f98169f35
Anton Tran <anton.tran33@gmail.com>
2025-09-13 13:59:47 +0200

    add CORS

--- src/main/java/dat250/experiment/pollapp/PollappApplication.java
+++ src/main/java/dat250/experiment/pollapp/PollappApplication.java
@@ -2,12 +2,27 @@ package dat250.experiment.pollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PollappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollappApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")   // Allow all endpoints
+						.allowedOrigins("*") // Allow all origins
+						.allowedMethods("*"); // Allow all HTTP methods (GET, POST, etc.)
+			}
+		};
 	}
 
 }
